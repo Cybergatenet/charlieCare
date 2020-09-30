20e395148ee2b1aca0ba1b10f8999eca4825616a
cybergate <cybergatenet@yahoo.com>
2020-09-30 09:27:18 -0700

    login form

--- login_signup/home.php
+++ login_signup/home.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+<?php require_once 'controllers/authcontroller.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,20 +13,30 @@
     <div class="container">
         <div class="row">
             <div class="col-md-4 offset-md-4 form-div login">
-                <div class="alert alert-success">
-                    You are now logged in!
+            <?php if(isset($_SESSION['msg'])): ?>
+                <div class="alert <?php echo $_SESSION['alert-class']; ?>">
+                    <?php 
+                        echo $_SESSION['msg'];
+                        unset($_SESSION['msg']);
+                        unset($_SESSION['alert-class']);
+                    ?>
                 </div>
+            <?php endif; ?>
 
-                <h3>Welcome, Cybergate</h3>
+                <h3>Welcome, <?php echo $_SESSION['username']; ?></h3>
 
-                <a href="#" class="logout">logout</a>
+                <a href="./index.html?logout=1" class="logout">logout</a>
 
-                <div class="alert alert-warning">
-                    You need to verify your account.
-                    Sign in to your email account and click on the verification link we just emailed you at <strong>cybergatenet@yahoo.com</strong>
-                </div>
+                <?php if(!$_SESSION['verified']): ?>
+                    <div class="alert alert-warning">
+                        You need to verify your account.
+                        Sign in to your email account and click on the verification link we just emailed you at <strong><?php echo $_SESSION['email']; ?></strong>
+                    </div>
+                <?php endif; ?>
 
-                <button class="btn btn-block btn-lg btn-primary">I am verified!</button>
+                <?php if($_SESSION['verified']): ?>
+                    <button class="btn btn-block btn-lg btn-primary">I am verified!</button>
+                <?php endif; ?>
             </div>
         </div>
     </div>    
@@ -36,43 +46,4 @@
     <script src="./js/jquery-1.9.1.min.js"></script>
     <script src="./js/bootstrap.min.js"></script>
 </body>
-=======
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home Page</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-    <link rel="stylesheet" href="./css/login.css">    
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 offset-md-4 form-div login">
-                <div class="alert alert-success">
-                    You are now logged in!
-                </div>
-
-                <h3>Welcome, Cybergate</h3>
-
-                <a href="#" class="logout">logout</a>
-
-                <div class="alert alert-warning">
-                    You need to verify your account.
-                    Sign in to your email account and click on the verification link we just emailed you at <strong>cybergatenet@yahoo.com</strong>
-                </div>
-
-                <button class="btn btn-block btn-lg btn-primary">I am verified!</button>
-            </div>
-        </div>
-    </div>    
-
-
-
-    <script src="./js/jquery-1.9.1.min.js"></script>
-    <script src="./js/bootstrap.min.js"></script>
-</body>
->>>>>>> 052988ad5309ced3aabec44f82b8adc30d30ada0
 </html>
