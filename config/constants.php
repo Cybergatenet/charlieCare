c9e5113a3894f2cde5579c31691414b64dead2c3
cybergate <cybergatenet@yahoo.com>
2020-10-11 23:13:47 -0700

    have to make thhis changes through a going to fix sysytem

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,9 @@
 <?php
-	define('ROOT_URL', 'http://www.charlycareclasic.com/');
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '123456');
-    define('DB_NAME', 'cyber_user');
+	// define('ROOT_URL', 'http://www.charlycareclasic.com/');
+    // define('DB_HOST', 'localhost');
+    // define('DB_USER', 'root');
+    // define('DB_PASS', '123456');
+    // define('DB_NAME', 'cyber_user');
     // define('DB_NAME', 'cybergate_user');
 
     ## origin variables
@@ -18,14 +18,14 @@
 
 
     ### second database
-    // define('DB_HOST', 'ec2-54-160-161-214.compute-1.amazonaws.com');
-    // define('DB_DATABASE', 'd735mp7tf0lee5');
-    // define('DB_USER', '
-    // miybpuvqhzxiie');
-    // define('DB_PASS', '45aaaa01572163c923524e84edfbf43cd4cab6b6d7cba257568425f09d6df109');
-    // define('DB_PORT', '
-    // 5432');
-    // define('DB_NAME', 'd18ng7n9fd505o');
-    // define('DB_URL', 'postgres://miybpuvqhzxiie:45aaaa01572163c923524e84edfbf43cd4cab6b6d7cba257568425f09d6df109@ec2-54-160-161-214.compute-1.amazonaws.com:5432/d735mp7tf0lee5');
-    // define('HEROKU_CLI', 'heroku pg:psql postgresql-closed-04594 --app charlycarecla');
+    define('DB_HOST', 'ec2-54-160-161-214.compute-1.amazonaws.com');
+    define('DB_DATABASE', 'd735mp7tf0lee5');
+    define('DB_USER', '
+    miybpuvqhzxiie');
+    define('DB_PASS', '45aaaa01572163c923524e84edfbf43cd4cab6b6d7cba257568425f09d6df109');
+    define('DB_PORT', '
+    5432');
+    define('DB_NAME', 'd18ng7n9fd505o');
+    define('DB_URL', 'postgres://miybpuvqhzxiie:45aaaa01572163c923524e84edfbf43cd4cab6b6d7cba257568425f09d6df109@ec2-54-160-161-214.compute-1.amazonaws.com:5432/d735mp7tf0lee5');
+    define('HEROKU_CLI', 'heroku pg:psql postgresql-closed-04594 --app charlycarecla');
 
--- config/db.php
+++ config/db.php
@@ -1,18 +1,18 @@
 <?php
 
-    // require 'constants.php';
+    require 'constants.php';
 
-    // $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
     // $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-    if(!$conn){
-        echo 'Database is not connnecting now';
-        header('location: ../../errors/404.php');
-        die($conn->connection_status);
-        exit("Oop!...NOT connected");
-    }
+    // if(!$conn){
+        // echo 'Database is not connnecting now. Try again in 24hours';
+        // header('location: ../../errors/404.php');
+        // die($conn->connection_status);
+        // exit("Oop!...NOT connected");
+    // }
 
     if($conn->connect_error) {
-        die('Database error-:)= could not connect to database' . $conn->connect_error);
+        die('Database error-:)= setting up dataBase in 24hours' . $conn->connect_error);
         header('location: ../../errors/404.php');
     }
