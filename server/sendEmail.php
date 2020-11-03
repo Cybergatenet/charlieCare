ca56aba20c4d1b337a2a1e21cc76f81b8d7a677a
Cybergatenet <abelchinedu2@gmail.com>
2020-11-03 11:05:50 +0100

    edited htaccess

--- server/sendEmail.php
+++ server/sendEmail.php
@@ -36,12 +36,13 @@ if(isset($_POST['contactMsg'])){
                 //Server settings
                 $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      // Enable verbose debug output
                 $mail->isSMTP();                                            // Send using SMTP
-                $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+                $mail->Host       = 'ssl://smtp.gmail.com';                    // Set the SMTP server to send through
                 $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
                 $mail->Username   = 'abelchinedu2@gmail.com';                     // SMTP username
                 $mail->Password   = 'uwasomba';                               // SMTP password
                 $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-                $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+                // $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+                $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
             
                 //Recipients
                 $mail->setFrom('abelchinedu2@gmail.com', 'Cybergate Testing');
