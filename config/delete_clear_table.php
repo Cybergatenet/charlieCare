92f062e996e559e8980f78d3049832c9c046d5be
Cybergatenet <abelchinedu2@gmail.com>
2020-11-10 03:09:03 +0100

    db up and running

--- config/delete_clear_table.php
+++ config/delete_clear_table.php
@@ -1,12 +1,12 @@
 <?php
-    require('./db.php');
+    // require('./db.php');
 
-	$table = "DROP TABLE `charlycare_users`";
+	// $table = "DROP TABLE `charlycare_users`#########################";
 
-	 if(mysqli_query($conn, $table) === false){
-            echo "CONNECTIOIN LOST: CONTACT ADMIN".'<br>'.mysqli_error($conn).'<br>'.$conn->connect_error;
-        }else{
-            echo "Table Deleted successfully";
-        }
+	//  if(mysqli_query($conn, $table) === false){
+    //         echo "CONNECTIOIN LOST: CONTACT ADMIN".'<br>'.mysqli_error($conn).'<br>'.$conn->connect_error;
+    //     }else{
+    //         echo "Table Deleted successfully";
+    //     }
 
 ?>
