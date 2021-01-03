898b815b0d92bc212d339399551d7ee22d02dcd7
Cybergatenet <abelchinedu2@gmail.com>
2021-01-03 08:31:53 +0100

    adding some files and fixing another image error

--- errors/404.php
+++ errors/404.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,7 +9,7 @@
     <meta name="keywords" content="CharlyCareCla$ic, Forex, Online Trading, Investment, Foreign exchange">
     <meta name="author" content="Designed by cybergate communication network">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Erro Page | CharlyCareCla$ic</title>
+    <title>Error Page | CharlyCareCla$ic</title>
     <!-- font-awesome cdn | locally hosted -->
     <link rel="icon" href="../img/charlyLogo22.png">
     <!-- fontAwesome here -->
@@ -19,18 +20,19 @@
     <link rel="stylesheet" type="text/css" href="../css/new_styles.css">
     <link rel="stylesheet" type="text/css" href="../css/styles.css">
     <style>
-        .hero{
-            width: 100%;
-            height: 100vh;
-            background: url(../img/error.png) center no-repeat;
-            background-size: 400px;
-            background-attachment: fixed;
-            display: flex;
-            align-items: center;
-            text-align: center;
-        }
+    .hero {
+        width: 100%;
+        height: 100vh;
+        background: url(../img/error.png) center no-repeat;
+        background-size: 400px;
+        background-attachment: fixed;
+        display: flex;
+        align-items: center;
+        text-align: center;
+    }
     </style>
 </head>
+
 <body>
     <?php include_once '../inc/header.php'; ?>
 
@@ -40,7 +42,8 @@
                 <span class="first-letter">404</span>Oop!
             </h2>
             <br><br><br><br><br>
-            <p style="color: #f00;" class="sub-headline typeWriter" data-wait="6000" data-words='["the life we deserve"]'>An Error has Occured</p>
+            <p style="color: #f00;" class="sub-headline typeWriter" data-wait="6000"
+                data-words='["the life we deserve"]'>An Error has Occured</p>
             <br><br><br><br><br>
             <p style="color: #f00;" class="sub-headline">We could not proces</p>
             <br><br><br><br><br>
@@ -48,7 +51,7 @@
             <br><br><br><br><br>
             <h1 class="headline"> Try Again</h1>
             <div class="headline-description">
-            <!-- deletesd from here -->
+                <!-- deletesd from here -->
                 <div class="single-animation">
                     <h5>To continue</h5>
                     <a href="../index.php" class="btn cta-btn">Click Here</a>
@@ -58,6 +61,7 @@
     </section>
     <!-- hero ends here -->
 
-	<script src="../js/main.js"></script>
+    <script src="../js/main.js"></script>
 </body>
+
 </html>
