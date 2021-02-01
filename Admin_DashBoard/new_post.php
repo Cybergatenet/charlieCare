d0f5964a181c50ec2cbf8a9f6e5c4973a83c478f
Cybergatenet <abelchinedu2@gmail.com>
2021-02-01 09:16:23 +0100

    second debugging

--- Admin_DashBoard/new_post.php
+++ Admin_DashBoard/new_post.php
@@ -13,8 +13,19 @@
     $element = trim($element);
     $element = htmlspecialchars($element);
     $element = htmlentities($element);
+    $element = mysqli_real_escape_string($conn, $element);
     return $element;
   }
+//   function fixtags($text){
+//     $text = htmlspecialchars($text);
+//     $text = preg_replace("/=/", "=\"\"", $text);
+//     $text = preg_replace("/&quot;/", "&quot;\"", $text);
+//     $tags = "/&lt;(\/|)(\w*)(\ |)(\w*)([\\\=]*)(?|(\")\"&quot;\"|)(?|(.*)?&quot;(\")|)([\ ]?)(\/|)&gt;/i";
+//     $replacement = "<$1$2$3$4$5$6$7$8$9$10>";
+//     $text = preg_replace($tags, $replacement, $text);
+//     $text = preg_replace("/=\"\"/", "=", $text);
+//     return $text;
+//     }
   ##########
   $msg = '';
   $msgClass = '';
