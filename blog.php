127bd837db5f8beebe6f9d61aa49a79d3576bc61
Cybergatenet <abelchinedu2@gmail.com>
2021-02-01 09:23:35 +0100

    third fix and maybe the final

--- blog.php
+++ blog.php
@@ -29,10 +29,14 @@ if(mysqli_num_rows($return_posts) > 0){
     function formatText($resolution){
         $resolution = trim($resolution);
         $resolution = nl2br($resolution);
-        $resolution = stripslashes($resolution);
+        // $resolution = stripslashes($resolution);
         // $resolution = htmlentities($resolution);
         // $resolution = htmlspecialchars($resolution);
-        $resolution = str_replace(array("&gt;", "&lt;", "&quot;", "&amp;", "&nbsp;"), array(">", "<", "\"", "&", ""), $resolution);
+
+        // $resolution = str_replace(array("&gt;", "&lt;", "&quot;", "&amp;", "&nbsp;"), array(">", "<", "\"", "&", ""), $resolution);
+
+        $resolution = html_entity_decode($resolution);
+
         // $resolution = str_replace(' r ',"", str_replace(' n ',"",$resolution));
         $resolution = str_replace('rnrn',"", str_replace('rn ',"",$resolution));
         // $resolution = str_replace(' r',"\r", str_replace('n ',"\n",$resolution));
